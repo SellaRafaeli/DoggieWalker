ae596af3893ce90e98e817969d71371e01ab23ae
Sella Rafaeli <sella.rafaeli@gmail.com>
2015-07-24 16:16:12 +0300

    app

---
+++ Gemfile
@@ -0,0 +1,4 @@
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'mongo'
+gem 'shotgun'
---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'erb'
+require 'mongo'
+
+#db
+$mongo = Mongo::MongoClient.from_uri('mongodb://localhost:27017').db('rails_girls')
+$tasks = $mongo.collection('tasks')
+
+#functions
+def view_tasks(tasks = nil)  
+  erb :view, locals: {tasks: $tasks.find.to_a}
+end
+
+get '/' do
+  view_tasks
+end
+
+post '/add' do
+  $tasks.insert(params)  
+  view_tasks
+end
+
+get '/delete' do
+  $tasks.remove(name: params[:name])
+  view_tasks
+end
+
