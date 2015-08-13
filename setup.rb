552f345400568a37a4c30ec1a4d91d28717be6cd
Sella Rafaeli <sella.rafaeli@gmail.com>
2015-08-13 16:57:41 +0300

    auto-message.

--- setup.rb
+++ setup.rb
@@ -2,7 +2,7 @@ set :views, Proc.new { File.join(root, "/") }
 set :public_folder, Proc.new { File.join(root, "/") }
 
 #db
-DB_NAME= ENV["MONGOLAB_DB_NAME"] || ('doggie-walker')
+DB_NAME= ENV["MONGOLAB_DBNAME"] || ('doggie-walker')
 DB_URI = ENV["MONGOLAB_URI"] || 'mongodb://localhost:27017'
 
 $mongo = Mongo::MongoClient.from_uri(DB_URI).db(DB_NAME)
